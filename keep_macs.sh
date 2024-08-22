db96923648d6da761559a170772773e95b1c433e
Allan Peda <allanpeda@users.noreply.github.com>
2024-08-22 19:37:06 -0400

    Update keep_macs.sh to use cmp

--- keep_macs.sh
+++ keep_macs.sh
@@ -24,19 +24,11 @@ a4:77:a5:a6:b5:b6
 a4:77:a5:a6:b5:b6
 EOF
 
-cmpsum(){
-   if [[ $(md5sum $1|awk '{print $1}') == $(md5sum $2|awk '{print $1}') ]]
-   then
-      return 0
-   fi
-   return 1
-}
-
 grep -f "${IPTMP}" /etc/tertf/tertfinfo_bak > "$TERTF"
 
 if [[ -s "$TERTF" ]]
 then
-   if cmpsum '/tmp/tertf/tertfinfo' '/tmp/tertf/tertfinfo_bak'
+   if cmp --silent '/tmp/tertf/tertfinfo' '/tmp/tertf/tertfinfo_bak'
    then
       cp -f "$TERTF" '/tmp/tertf/tertfinfo'
       cp -f "$TERTF" '/tmp/tertf/tertfinfo_bak'
